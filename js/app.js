88c10eba55f96d014526b225b1b0dcaf7e89c54b
Matias Salazar <matias39974593@gmail.com>
2024-07-26 18:54:39 -0300

    vimos como insertar un nuevo elemento

--- js/app.js
+++ js/app.js
@@ -12,21 +12,30 @@ const cambiarTitulo = () => {
   tituloPrincipal.className = "text-info";
 
   //cambiar el h1, texto y style
+};
 
-}
+const crearParrafo = () => {
+  console.log("desde crearParrafo");
+  //crear un nuevo parrafo
+  const parrafoNuevo = document.createElement("p");
+  parrafoNuevo.innerHTML =
+    "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dicta totam neque iure nesciunt unde quo id, officia dolorum blanditiis ducimus! Possimus cum, quos dolores deleniti in rem ipsa sed porro iste repellat dolorum, obcaecati minus nesciunt excepturi asperiores hic maiores architecto ex tempora repudiandae fugiat veniam. Harum quae laboriosam maiores.";
+  parrafoNuevo.className = "lead";
+  console.log(parrafoNuevo);
 
-const crearParrafo = () =>{
-    console.log('desde crearParrafo')
-    //crear un nuevo parrafo 
+  // insertar  el parrafo nuevo en el segundo section
+  const secciones = document.querySelectorAll("section");
+  // insertar el nuevo elemento
+  //secciones[1].appendChild(parrafoNuevo); //agrega el nodo al final
+  //secciones[1].prepend(parrafoNuevo) // agrega antes del h1 (arriba)
+  secciones[1].insertBefore(parrafoNuevo, btnVermas)
+};
 
-    // insertar  el parrafo nuevo en el segundo section
-}
+// agregar un evento al elemento del HTML
+const btnVermas = document.querySelector(".btn-outline-info");
+console.log(btnVermas);
 
-// agregar un evento al elemento del HTML 
-const btnVermas = document.querySelector('.btn-outline-info');
-console.log(btnVermas)
-
-// manejadores de eventos 
+// manejadores de eventos
 //cuando tengo una funcion con parametros, hacer:
 //btnVermas.addEventListener('click', ()=> crearParrafo(algoaqui))
-btnVermas.addEventListener('click', crearParrafo)
+btnVermas.addEventListener("click", crearParrafo);
