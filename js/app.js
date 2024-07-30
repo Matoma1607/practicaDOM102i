5568bc7153b47644bccd66bf6f469f1117f6f2a5
Matias Salazar <matias39974593@gmail.com>
2024-07-30 13:17:56 -0300

    agregamos un condicional para eliminar un parrafo y vimos un ejemplo de submit

--- js/app.js
+++ js/app.js
@@ -1,3 +1,12 @@
+let mostrarParrafo = true;
+const buscarPalabra = (e) => {
+  e.preventDefault();
+  console.log(e);
+  console.log("desde la funcion buscarPalabra");
+  //buscar el valor del input
+  const input = document.querySelector(".form-control")
+  console.log(input.value)
+};
 const cambiarTitulo = () => {
   console.log("desde la funcion cambiarTitulo");
   //obtener el h1 del proyecto
@@ -15,27 +24,50 @@ const cambiarTitulo = () => {
 };
 
 const crearParrafo = () => {
-  console.log("desde crearParrafo");
-  //crear un nuevo parrafo
-  const parrafoNuevo = document.createElement("p");
-  parrafoNuevo.innerHTML =
-    "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dicta totam neque iure nesciunt unde quo id, officia dolorum blanditiis ducimus! Possimus cum, quos dolores deleniti in rem ipsa sed porro iste repellat dolorum, obcaecati minus nesciunt excepturi asperiores hic maiores architecto ex tempora repudiandae fugiat veniam. Harum quae laboriosam maiores.";
-  parrafoNuevo.className = "lead";
-  console.log(parrafoNuevo);
-
-  // insertar  el parrafo nuevo en el segundo section
+  //insertar  el parrafo nuevo en el segundo section
   const secciones = document.querySelectorAll("section");
-  // insertar el nuevo elemento
-  //secciones[1].appendChild(parrafoNuevo); //agrega el nodo al final
-  //secciones[1].prepend(parrafoNuevo) // agrega antes del h1 (arriba)
-  secciones[1].insertBefore(parrafoNuevo, btnVermas)
+  if (mostrarParrafo) {
+    console.log("desde crearParrafo");
+    //crear un nuevo parrafo
+    const parrafoNuevo = document.createElement("p");
+    parrafoNuevo.innerHTML =
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dicta totam neque iure nesciunt unde quo id, officia dolorum blanditiis ducimus! Possimus cum, quos dolores deleniti in rem ipsa sed porro iste repellat dolorum, obcaecati minus nesciunt excepturi asperiores hic maiores architecto ex tempora repudiandae fugiat veniam. Harum quae laboriosam maiores.";
+    parrafoNuevo.className = "lead";
+    console.log(parrafoNuevo);
+
+    // insertar el nuevo elemento
+    //secciones[1].appendChild(parrafoNuevo); //agrega el nodo al final
+    //secciones[1].prepend(parrafoNuevo) // agrega antes del h1 (arriba)
+    secciones[1].insertBefore(parrafoNuevo, btnVermas);
+    //cambiar el texto del boton
+    btnVermas.innerHTML = "ver menos";
+    //btnVermas.className = 'btn btn-outline-danger'
+    btnVermas.classList.remove("btn-outline-info");
+    btnVermas.classList.add("btn-outline-danger");
+    // cambio mi variable bandera
+    mostrarParrafo = false;
+  } else {
+    console.log("aqui borra el parrafo");
+    console.log(secciones[1].children);
+    //eliminar un nodo
+    secciones[1].removeChild(secciones[1].children[2]);
+    //resetear el boton verMas
+    mostrarParrafo = true;
+    btnVermas.classList.remove("btn-outline-danger");
+    btnVermas.classList.add("btn-outline-info");
+    btnVermas.innerHTML = "ver mas...";
+  }
 };
 
 // agregar un evento al elemento del HTML
 const btnVermas = document.querySelector(".btn-outline-info");
 console.log(btnVermas);
+// traer el formulario
+const formularioBusqueda = document.querySelector(".d-flex");
 
 // manejadores de eventos
 //cuando tengo una funcion con parametros, hacer:
 //btnVermas.addEventListener('click', ()=> crearParrafo(algoaqui))
 btnVermas.addEventListener("click", crearParrafo);
+
+formularioBusqueda.addEventListener("submit", buscarPalabra);
